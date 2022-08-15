a356571eeaae8095dd9a1ac67e3bce2d279dc727
leojacoby <leojacoby@gmail.com>
2022-08-15 17:30:05 -0400

    update copy

--- utils/content.ts
+++ utils/content.ts
@@ -7,29 +7,25 @@ export interface MetaProps {
     type?: string;
 }
 
-const description = 'An NFT only mintable by the real ones';
+const description = 'Thank you from all of us.';
 
 export const headMetadata: MetaProps = {
-    title: 'Heartbeat',
+    title: 'Nomad whitehat',
     description,
     image: `https://${WEBSITE_URL}/site-preview.png`,
     type: 'website',
 };
 
 export const copy = {
-    title: 'Welcome, whitehats',
-    nameLowercase: 'heartbeat',
+    title: 'Nomad whitehat',
     heroSubheading: description,
+    nameLowercase: 'nomad whitehat',
     heading1: 'White Hat Prize',
     text1: `Reputation and contribution are the lifeblood of a sustainable crypto
                         revolution. We're here to offer our thanks to anyone who returns 90% or
                         more of their withdrawal from the Nomad Bridge during the Bridge hack Claim
                         an NFT endorsed by Nomad that proves your on-chain return of 90%+ funds
                         after your withdrawal.`,
-    heading2: 'Multi-chain',
-    text2: 'Activity is tracked across Ethereum, Polygon, Fantom, and Avalanche.',
-    heading3: 'Earned Attributes',
-    text3: 'The layers, speed, colors, and spikes are each based on a different length of time. What can you do to make yours more unique?',
     bottomSectonHeading: 'EARNED NFTS ARE THE FUTURE',
     bottomSectionText: `Earned NFTs reward the best in crypto communities, granting them access, permissions and status that can’t be bought. They crystallize our achievements, contributions, sacrifices and convictions and help others know who is more trustworthy, more knowledgeable, more helpful. Interested in bringing these to your community?`,
     metagameCta: 'Play the game',
